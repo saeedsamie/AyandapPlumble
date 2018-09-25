e7003e0a03f188d570034a803438e968e5f1fbe6
Saeed <saeed.samie.74@gmail.com>
2018-09-25 08:46:37 +0330

    Remove notations usage @notNull @nullable

--- app/src/main/java/com/morlunk/mumbleclient/app/RecentChatsListAdapter.java
+++ app/src/main/java/com/morlunk/mumbleclient/app/RecentChatsListAdapter.java
@@ -1,10 +1,6 @@
 package com.morlunk.mumbleclient.app;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,74 +10,69 @@ import android.widget.TextView;
 
 import com.morlunk.mumbleclient.R;
 
-import org.jetbrains.annotations.Nullable;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 
 public class RecentChatsListAdapter extends BaseAdapter {
 
-  Context context;
-  private final ArrayList<HashMap<String, String>> values;
+    private final ArrayList<HashMap<String, String>> values;
+    Context context;
 
-  public RecentChatsListAdapter(Context context, ArrayList<HashMap<String, String>> values) {
-    //super(context, R.layout.single_list_app_item, utilsArrayList);
-    this.context = context;
-    this.values = values;
-  }
+    public RecentChatsListAdapter(Context context, ArrayList<HashMap<String, String>> values) {
+        //super(context, R.layout.single_list_app_item, utilsArrayList);
+        this.context = context;
+        this.values = values;
+    }
 
-  @Override
-  public int getCount() {
-    return values.size();
-  }
+    @Override
+    public int getCount() {
+        return values.size();
+    }
 
-  @Override
-  public Object getItem(int i) {
-    return i;
-  }
+    @Override
+    public Object getItem(int i) {
+        return i;
+    }
 
-  @Override
-  public long getItemId(int i) {
-    return i;
-  }
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
 
-  //
-  @Override
-  public View getView(int position, View convertView, ViewGroup parent) {
+    //
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
 
 
-    ViewHolder viewHolder;
+        ViewHolder viewHolder;
 
-    final View result;
+        final View result;
 
-    if (convertView == null) {
+        if (convertView == null) {
 
-      viewHolder = new ViewHolder();
-      LayoutInflater inflater = LayoutInflater.from(context);
-      convertView = inflater.inflate(R.layout.chat_row, parent, false);
-      viewHolder.txtName = (TextView) convertView.findViewById(R.id.recent_chats_list_row_title);
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(context);
+            convertView = inflater.inflate(R.layout.chat_row, parent, false);
+            viewHolder.txtName = (TextView) convertView.findViewById(R.id.recent_chats_list_row_title);
 //      viewHolder.icon = (ImageView) convertView.findViewById(R.id.recent_chats_list_row_image);
 
-      result = convertView;
-      convertView.setTag(viewHolder);
-    } else {
-      viewHolder = (ViewHolder) convertView.getTag();
-      result = convertView;
+            result = convertView;
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+            result = convertView;
+        }
+        viewHolder.txtName.setText(values.get(position).get("ChatTitle"));
+        return convertView;
     }
-    viewHolder.txtName.setText(values.get(position).get("ChatTitle"));
-    return convertView;
-  }
 
-  //
-  private static class ViewHolder {
+    //
+    private static class ViewHolder {
 
-    TextView txtName;
-    ImageView icon;
-  }
+        TextView txtName;
+        ImageView icon;
+    }
 //  public class AsyncTaskLoadImage  extends AsyncTask<String, String, Bitmap> {
 //    private final static String TAG = "AsyncTaskLoadImage";
 //    private ImageView imageView;
